1d73ba2631686ea6824a1cb03411babc9d15cae9
William Young <fractalwizz@hotmail.com>
2015-06-10 00:04:58 -0500

    First Commit - Weather

---
+++ app/src/main/java/com/fract/nano/williamyoung/sunshine/MainActivityFragment.java
@@ -0,0 +1,38 @@
+package com.fract.nano.williamyoung.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Today - Sunny - 88 / 33");
+        list.add("Tomorrow - Sunny - 86 / 45");
+        list.add("Tues - Cloudy - 81 / 55");
+        list.add("Wed - Rainy - 71 / 33");
+        list.add("Thur - Stormy - 66 / 23");
+        list.add("Fri - Snowy - 10 / -3");
+        list.add("Sat - Sunny - 89 - 70");
+
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, list);
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
