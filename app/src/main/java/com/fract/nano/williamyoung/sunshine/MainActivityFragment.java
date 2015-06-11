e10be39ff984cebe141e7b14bbd0b6d696b3f877
William Young <fractalwizz@hotmail.com>
2015-06-11 02:22:25 -0500

    Commit #3 - UpdateInProgress

--- app/src/main/java/com/fract/nano/williamyoung/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/fract/nano/williamyoung/sunshine/MainActivityFragment.java
@@ -1,13 +1,25 @@
 package com.fract.nano.williamyoung.sunshine;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -17,6 +29,33 @@ import java.util.ArrayList;
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            new FetchWeatherTask().execute("68105");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -38,5 +77,82 @@ public class MainActivityFragment extends Fragment {
         lv.setAdapter(adapt);
 
         return root;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, Void>{
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(String... postcode){
+
+            if (postcode.length == 0)
+            {
+                return null;
+            }
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonStr = null;
+
+            try{
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("http")
+                        .authority("api.openweathermap.org")
+                        .appendPath("data")
+                        .appendPath("2.5")
+                        .appendPath("forecast")
+                        .appendPath("daily")
+                        .appendQueryParameter("q", postcode.toString())
+                        .appendQueryParameter("mode", "json")
+                        .appendQueryParameter("units", "metric")
+                        .appendQueryParameter("cnt", "7");
+                URL url = new URL(builder.build().toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null)
+                {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null)
+                {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0)
+                {
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e){
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally{
+                if (urlConnection != null)
+                {
+                    urlConnection.disconnect();
+                }
+                if (reader != null)
+                {
+                    try {
+                        reader.close();
+                    } catch (final IOException e){
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
