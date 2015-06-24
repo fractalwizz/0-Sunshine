a618c15172af6eb0249eaf8c0586ea6d4c0b1592
William Young <fractalwizz@hotmail.com>
2015-06-24 10:59:41 -0500

    Commit #6 - Testing, DB, assertions

--- app/src/main/java/com/fract/nano/williamyoung/sunshine/MainActivity.java
+++ app/src/main/java/com/fract/nano/williamyoung/sunshine/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -15,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
